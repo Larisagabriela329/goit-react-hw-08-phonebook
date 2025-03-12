88ad109cc32e42df23323edd85b3aacbc6ff1acb
Larisagabriela329 <larisagabriela329@yahoo.com>
2025-03-12 13:23:23 +0200

    Initial commit

---
+++ src/components/Filter.jsx
@@ -0,0 +1,23 @@
+import PropTypes from 'prop-types';
+
+const Filter = ({ value, onChange }) => (
+  <div>
+    <label>
+      Find contacts by Name
+      <input
+        type="text"
+        value={value}
+        onChange={e => onChange(e.target.value)}
+        style={{  marginLeft: '10px' }}
+      />
+    </label>
+  </div>
+);
+
+Filter.propTypes = {
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+};
+
+export default Filter;
+
